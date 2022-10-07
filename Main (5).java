c29225236e57477354e34b1ba16b014afd8b3ba7
israel203010 <111544955+israel203010@users.noreply.github.com>
2022-10-06 21:22:10 -0300

    Add files via upload

---
+++ Main (5).java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Main {
+  public static void main(String[] args) {
+    Scanner inicio = new Scanner (System.in);
+    int a, b;
+    System.out.println("por favor digite o valor de a");
+    a = entrada.nextInt();
+    System.out.println ();
+
+    for (b=1;  b<=a; i++){
+      if ((b%2) ==1){
+      System.out.println(b);
+    
+  }
+}
+  }
+}
